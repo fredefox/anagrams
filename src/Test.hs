c6754923f0e48d39222e6a598dfaed09363df680
Frederik Hanghøj Iversen <fhi.1990@gmail.com>
2016-06-18 13:13:51 +0200

    Make testing properties polymorphic

--- src/Test.hs
+++ src/Test.hs
@@ -9,11 +9,11 @@ import Data.List
 -- TODO: Should we arbitrarily constrain the test-cases to @String@s?
 
 {-| The found anagrams exists in the dictionary -}
-prop_anagramsExist :: [String] -> String -> Bool
+prop_anagramsExist :: Eq a => [[a]] -> [a] -> Bool
 prop_anagramsExist dict = all (`elem` dict) . anagrams dict
 
 {-| An anagram of @s@ have contain the same elements in any order -}
-prop_anagramInvariant :: [String] -> String -> Bool
+prop_anagramInvariant :: Eq a => [[a]] -> [a] -> Bool
 prop_anagramInvariant dict w = all (=~ w) . anagrams dict $ w where
     x =~ y = null $ x \\ y
 
